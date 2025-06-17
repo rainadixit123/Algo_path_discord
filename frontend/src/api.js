150308b6e22a7b6a2b11a91c87045fb92fae513a
Raina Dixit <rainadixit11048@gmail.com>
2025-06-17 23:19:35 +0530

    deploy-commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://algodiscord-backend.onrender.com/api";
-// const BASE_URL="http://localhost:5000"
+ const BASE_URL=process.env.REACT_APP_API_BASE_URL;
 export const requestOtp = async (email) => {
   const res = await axios.post(`${BASE_URL}/request-otp`, { email });
   return res.data;
